b13c67ff0e0fe0bf2d7ae23629a9f315b0e04523
kowarschick <WOMAsisohg5>
2020-07-16 11:40:07 +0200

    coverage works

--- jest.config.js
+++ jest.config.js
@@ -1,22 +1,29 @@
 module.exports = 
-{ rootDir:         'test',
+{ rootDir:         '.',
   testRegex:       '^[^\.]+\.ts$',
-  testEnvironment: 'node',
-  preset:          'ts-jest',
-  resolver:        null,
 
-  globals: 
-  { 'ts-jest': { tsconfig: './tsconfig.json' } },
+  transform:
+  {"^.+\\.ts$": "ts-jest"},
 
   moduleFileExtensions: 
-  ["js", "ts"],
+  [ "js", "ts" ],
 
   moduleNameMapper: 
-  { '^~/(.*)':  '<rootDir>/../src/$1' },
-  
-  collectCoverageFrom: 
-  [ "./**/*.ts" ],
+  { '^~/(.*)':  '<rootDir>/src/$1' },
 
-  coverageReporters: 
-  ['json', 'lcov']
+  testPathIgnorePatterns:
+  [ '/node_modules/',
+    '/src/',
+    'examples_cjs',
+    'examples_es6',
+    'docs_js',
+    'docs_ts',
+    'coverage',
+    'tmp',
+    'transformer',
+    'transformer.commented'
+  ],
+
+  coverageReporters:
+  [ 'lcov', 'text' ]
 };

6b16d5a789a60dcd3893094756c3491e771cb3b4
kowa <kowa@hs-augsburg.de>
2020-07-18 17:07:39 +0200

    examples-es6: compression improved

--- webpack.config.examples_es6.js
+++ webpack.config.examples_es6.js
@@ -1,4 +1,6 @@
-const  path = require('path');
+const
+  path = require('path'),
+  TerserPlugin = require('terser-webpack-plugin');
 
 function f_path(l_path)
 { return path.resolve(__dirname, l_path); }
@@ -6,6 +8,8 @@ function f_path(l_path)
 module.exports = 
 { entry:  [f_path('examples_es6') + '/examples_es6.js'],
   target: 'node',
+
+  mode: "production",
 
   resolve: 
   { extensions: [ '.js' ],
@@ -15,5 +19,10 @@ module.exports =
   output: 
   { filename: 'examples_bundle_es6.js',
     path: f_path('./examples_es6')
+  },
+
+  optimization: 
+  { minimize: true,
+    minimizer: [ new TerserPlugin() ],
   },
 }
